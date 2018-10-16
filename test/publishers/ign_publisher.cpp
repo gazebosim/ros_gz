0be4f5628beccb7334a22915b308e0b66689090b
Carlos Aguero <caguero@osrfoundation.org>
2018-10-16 15:55:59 -0700

    Tests Ign->ROS1.

--- test/publishers/ign_publisher.cpp
+++ test/publishers/ign_publisher.cpp
@@ -21,6 +21,7 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <ignition/common/Image.hh>
 #include <ignition/msgs.hh>
 #include <ignition/transport.hh>
 
@@ -46,28 +47,88 @@ int main(int /*argc*/, char **/*argv*/)
 
   // Create a transport node and advertise a topic.
   ignition::transport::Node node;
-  std::string topic = "/chatter";
 
-  auto pub = node.Advertise<ignition::msgs::StringMsg>(topic);
-  if (!pub)
+  // ignition::msgs::Header.
+  auto header_pub = node.Advertise<ignition::msgs::Header>("header");
+  ignition::msgs::Header header_msg;
+  header_msg.mutable_stamp()->set_sec(2);
+  header_msg.mutable_stamp()->set_nsec(3);
+
+  // ignition::msgs::StringMsg.
+  auto string_pub = node.Advertise<ignition::msgs::StringMsg>("string");
+  ignition::msgs::StringMsg string_msg;
+  string_msg.set_data("string");
+
+  // ignition::msgs::Quaternion.
+  auto quaternion_pub =
+    node.Advertise<ignition::msgs::Quaternion>("quaternion");
+  ignition::msgs::Quaternion quaternion_msg;
+  quaternion_msg.set_x(1.0);
+  quaternion_msg.set_y(2.0);
+  quaternion_msg.set_z(3.0);
+  quaternion_msg.set_w(4.0);
+
+  // ignition::msgs::Vector3d.
+  auto vector3_pub = node.Advertise<ignition::msgs::Vector3d>("vector3");
+  ignition::msgs::Vector3d vector3_msg;
+  vector3_msg.set_x(1.0);
+  vector3_msg.set_y(2.0);
+  vector3_msg.set_z(3.0);
+
+  // ignition::msgs::Image.
+  auto image_pub = node.Advertise<ignition::msgs::Image>("image");
+  ignition::msgs::Image image_msg;
+  image_msg.mutable_header()->CopyFrom(header_msg);
+  image_msg.set_width(320);
+  image_msg.set_height(240);
+  image_msg.set_pixel_format(
+    ignition::common::Image::PixelFormatType::RGB_INT8);
+  image_msg.set_step(image_msg.width() * 3);
+  image_msg.set_data(std::string(image_msg.height() * image_msg.step(), '0'));
+
+  // ignition::msgs::IMU.
+  auto imu_pub = node.Advertise<ignition::msgs::IMU>("imu");
+  ignition::msgs::IMU imu_msg;
+  imu_msg.mutable_header()->CopyFrom(header_msg);
+  imu_msg.mutable_orientation()->CopyFrom(quaternion_msg);
+  imu_msg.mutable_angular_velocity()->CopyFrom(vector3_msg);
+  imu_msg.mutable_linear_acceleration()->CopyFrom(vector3_msg);
+
+  // ignition::msgs::LaserScan.
+  auto laserscan_pub = node.Advertise<ignition::msgs::LaserScan>("laserscan");
+  const unsigned int num_readings = 100u;
+  ignition::msgs::LaserScan laserscan_msg;
+  laserscan_msg.mutable_header()->CopyFrom(header_msg);
+  laserscan_msg.set_angle_min(-1.57);
+  laserscan_msg.set_angle_max(1.57);
+  laserscan_msg.set_angle_step(3.14 / num_readings);
+  laserscan_msg.set_range_min(1);
+  laserscan_msg.set_range_max(2);
+  laserscan_msg.set_count(num_readings);
+  laserscan_msg.set_vertical_angle_min(0);
+  laserscan_msg.set_vertical_angle_max(0);
+  laserscan_msg.set_vertical_angle_step(0);
+  laserscan_msg.set_vertical_count(0);
+
+  for (auto i = 0u; i < laserscan_msg.count(); ++i)
   {
-    std::cerr << "Error advertising topic [" << topic << "]" << std::endl;
-    return -1;
+    laserscan_msg.add_ranges(0);
+    laserscan_msg.add_intensities(1);
   }
-
-  // Prepare the message.
-  ignition::msgs::StringMsg msg;
-  msg.set_data("hello");
 
   // Publish messages at 1Hz.
   while (!g_terminatePub)
   {
-    if (!pub.Publish(msg))
-      break;
+    header_pub.Publish(header_msg);
+    string_pub.Publish(string_msg);
+    quaternion_pub.Publish(quaternion_msg);
+    vector3_pub.Publish(vector3_msg);
+    image_pub.Publish(image_msg);
+    imu_pub.Publish(imu_msg);
+    laserscan_pub.Publish(laserscan_msg);
 
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
   return 0;
 }
-
--- test/subscribers/ros1_subscriber.cpp
+++ test/subscribers/ros1_subscriber.cpp
@@ -17,7 +17,13 @@
 
 #include <gtest/gtest.h>
 #include <ros/ros.h>
+#include <std_msgs/Header.h>
 #include <std_msgs/String.h>
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/Vector3.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/LaserScan.h>
 #include <chrono>
 #include "../test_config.h"
 
@@ -32,14 +38,133 @@ class MyTestClass
   }
 
   /// \brief Create a subscriber.
-  public: void Subscribe()
+  public: void SubscribeHeader()
   {
-    this->sub = this->n.subscribe("chatter", 1000, &MyTestClass::Cb, this);
+    this->sub = this->n.subscribe("header", 1000, &MyTestClass::HeaderCb, this);
   }
 
   /// \brief Member function called each time a topic update is received.
-  public: void Cb(const std_msgs::String::ConstPtr& /*msg*/)
+  public: void HeaderCb(const std_msgs::Header::ConstPtr& msg)
   {
+    EXPECT_EQ(2, msg->stamp.sec);
+    EXPECT_EQ(3, msg->stamp.nsec);
+    this->callbackExecuted = true;
+  };
+
+  /// \brief Create a subscriber.
+  public: void SubscribeString()
+  {
+    this->sub = this->n.subscribe("string", 1000, &MyTestClass::StringCb, this);
+  }
+
+  /// \brief Member function called each time a topic update is received.
+  public: void StringCb(const std_msgs::String::ConstPtr& msg)
+  {
+    EXPECT_EQ("string", msg->data);
+    this->callbackExecuted = true;
+  };
+
+  /// \brief Create a subscriber.
+  public: void SubscribeQuaternion()
+  {
+    this->sub = this->n.subscribe(
+      "quaternion", 1000, &MyTestClass::QuaternionCb, this);
+  }
+
+  /// \brief Member function called each time a topic update is received.
+  public: void QuaternionCb(const geometry_msgs::Quaternion::ConstPtr& msg)
+  {
+    EXPECT_EQ(1, msg->x);
+    EXPECT_EQ(2, msg->y);
+    EXPECT_EQ(3, msg->z);
+    EXPECT_EQ(4, msg->w);
+    this->callbackExecuted = true;
+  };
+
+  /// \brief Create a subscriber.
+  public: void SubscribeVector3()
+  {
+    this->sub = this->n.subscribe(
+      "vector3", 1000, &MyTestClass::Vector3Cb, this);
+  }
+
+  /// \brief Member function called each time a topic update is received.
+  public: void Vector3Cb(const geometry_msgs::Vector3::ConstPtr& msg)
+  {
+    EXPECT_EQ(1, msg->x);
+    EXPECT_EQ(2, msg->y);
+    EXPECT_EQ(3, msg->z);
+    this->callbackExecuted = true;
+  };
+
+  /// \brief Create a subscriber.
+  public: void SubscribeImage()
+  {
+    this->sub = this->n.subscribe("image", 1000, &MyTestClass::ImageCb, this);
+  }
+
+  /// \brief Member function called each time a topic update is received.
+  public: void ImageCb(const sensor_msgs::Image::ConstPtr& msg)
+  {
+    const unsigned int expected_width = 320u;
+    const unsigned int expected_height = 240u;
+    EXPECT_EQ(2, msg->header.stamp.sec);
+    EXPECT_EQ(3, msg->header.stamp.nsec);
+    EXPECT_EQ(expected_width, msg->width);
+    EXPECT_EQ(expected_height, msg->height);
+    EXPECT_EQ("rgb8", msg->encoding);
+    EXPECT_FALSE(msg->is_bigendian);
+    EXPECT_EQ(expected_width * 3, msg->step);
+
+    this->callbackExecuted = true;
+  };
+
+  /// \brief Create a subscriber.
+  public: void SubscribeImu()
+  {
+    this->sub = this->n.subscribe("imu", 1000, &MyTestClass::ImuCb, this);
+  }
+
+  /// \brief Member function called each time a topic update is received.
+  public: void ImuCb(const sensor_msgs::Imu::ConstPtr& msg)
+  {
+    EXPECT_EQ(2, msg->header.stamp.sec);
+    EXPECT_EQ(3, msg->header.stamp.nsec);
+    EXPECT_EQ(1, msg->orientation.x);
+    EXPECT_EQ(2, msg->orientation.y);
+    EXPECT_EQ(3, msg->orientation.z);
+    EXPECT_EQ(4, msg->orientation.w);
+    EXPECT_EQ(1, msg->angular_velocity.x);
+    EXPECT_EQ(2, msg->angular_velocity.y);
+    EXPECT_EQ(3, msg->angular_velocity.z);
+    EXPECT_EQ(1, msg->linear_acceleration.x);
+    EXPECT_EQ(2, msg->linear_acceleration.y);
+    EXPECT_EQ(3, msg->linear_acceleration.z);
+
+    this->callbackExecuted = true;
+  };
+
+  /// \brief Create a subscriber.
+  public: void SubscribeLaserScan()
+  {
+    this->sub = this->n.subscribe(
+      "laserscan", 1000, &MyTestClass::LaserScanCb, this);
+  }
+
+  /// \brief Member function called each time a topic update is received.
+  public: void LaserScanCb(const sensor_msgs::LaserScan::ConstPtr& msg)
+  {
+    const unsigned int expected_num_readings = 100u;
+    EXPECT_EQ(2, msg->header.stamp.sec);
+    EXPECT_EQ(3, msg->header.stamp.nsec);
+    EXPECT_FLOAT_EQ(-1.57, msg->angle_min);
+    EXPECT_FLOAT_EQ(1.57, msg->angle_max);
+    EXPECT_FLOAT_EQ(3.14 / expected_num_readings, msg->angle_increment);
+    EXPECT_FLOAT_EQ(0, msg->time_increment);
+    EXPECT_FLOAT_EQ(0, msg->scan_time);
+    EXPECT_FLOAT_EQ(1, msg->range_min);
+    EXPECT_FLOAT_EQ(2, msg->range_max);
+
     this->callbackExecuted = true;
   };
 
@@ -59,10 +184,82 @@ class MyTestClass
 };
 
 /////////////////////////////////////////////////
-TEST(ROS1StringSubscriberTest, String)
+TEST(ROS1SubscriberTest, Header)
 {
   MyTestClass client;
-  client.Subscribe();
+  client.SubscribeHeader();
+
+  using namespace std::chrono_literals;
+  ros1_ign_bridge::waitUntilBoolVarAndSpin(client.callbackExecuted, 10ms, 200);
+
+  EXPECT_TRUE(client.callbackExecuted);
+}
+
+/////////////////////////////////////////////////
+TEST(ROS1SubscriberTest, String)
+{
+  MyTestClass client;
+  client.SubscribeString();
+
+  using namespace std::chrono_literals;
+  ros1_ign_bridge::waitUntilBoolVarAndSpin(client.callbackExecuted, 10ms, 200);
+
+  EXPECT_TRUE(client.callbackExecuted);
+}
+
+/////////////////////////////////////////////////
+TEST(ROS1SubscriberTest, Quaternion)
+{
+  MyTestClass client;
+  client.SubscribeQuaternion();
+
+  using namespace std::chrono_literals;
+  ros1_ign_bridge::waitUntilBoolVarAndSpin(client.callbackExecuted, 10ms, 200);
+
+  EXPECT_TRUE(client.callbackExecuted);
+}
+
+/////////////////////////////////////////////////
+TEST(ROS1SubscriberTest, Vector3)
+{
+  MyTestClass client;
+  client.SubscribeVector3();
+
+  using namespace std::chrono_literals;
+  ros1_ign_bridge::waitUntilBoolVarAndSpin(client.callbackExecuted, 10ms, 200);
+
+  EXPECT_TRUE(client.callbackExecuted);
+}
+
+/////////////////////////////////////////////////
+TEST(ROS1SubscriberTest, Image)
+{
+  MyTestClass client;
+  client.SubscribeImage();
+
+  using namespace std::chrono_literals;
+  ros1_ign_bridge::waitUntilBoolVarAndSpin(client.callbackExecuted, 10ms, 200);
+
+  EXPECT_TRUE(client.callbackExecuted);
+}
+
+/////////////////////////////////////////////////
+TEST(ROS1SubscriberTest, Imu)
+{
+  MyTestClass client;
+  client.SubscribeImu();
+
+  using namespace std::chrono_literals;
+  ros1_ign_bridge::waitUntilBoolVarAndSpin(client.callbackExecuted, 10ms, 200);
+
+  EXPECT_TRUE(client.callbackExecuted);
+}
+
+/////////////////////////////////////////////////
+TEST(ROS1SubscriberTest, LaserScan)
+{
+  MyTestClass client;
+  client.SubscribeLaserScan();
 
   using namespace std::chrono_literals;
   ros1_ign_bridge::waitUntilBoolVarAndSpin(client.callbackExecuted, 10ms, 200);
