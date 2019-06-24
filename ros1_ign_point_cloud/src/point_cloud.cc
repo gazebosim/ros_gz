2ecd6a2677656e8724da62483307005230f307d7
chapulina <louise@openrobotics.org>
2019-06-24 13:14:11 -0700

    PC2 for gpu_lidar, 1 vertical sample

--- ros1_ign_point_cloud/src/point_cloud.cc
+++ ros1_ign_point_cloud/src/point_cloud.cc
@@ -45,7 +45,7 @@ using namespace ros1_ign_point_cloud;
 enum class SensorType {
   /// \brief A camera which combines an RGB and a depth camera
   RGBD_CAMERA,
- 
+
   /// \brief Depth camera
   DEPTH_CAMERA,
 
@@ -66,18 +66,6 @@ class ros1_ign_point_cloud::PointCloudPrivate
   public: void OnNewDepthFrame(const float *_scan,
             unsigned int _width, unsigned int _height,
             unsigned int _channels,
-            const std::string &_format);
-
-  /// \brief Callback when the GPU rays generate a new frame.
-  /// This is called in the rendering thread.
-  /// \param[in] _frame Scan data
-  /// \param[in] _width Image width in pixels
-  /// \param[in] _height Image height in pixels
-  /// \param[in] _channels Number of channels in image.
-  /// \param[in] _format Image format as string.
-  public: void OnNewGpuRaysFrame(const float *_frame,
-            unsigned int _width, unsigned int _height,
-            unsigned int _depth,
             const std::string &_format);
 
   /// \brief Get depth camera from rendering.
@@ -322,7 +310,7 @@ void PointCloudPrivate::LoadGpuRays(
   }
 
   this->gpu_rays_connection_ = this->gpu_rays_->ConnectNewGpuRaysFrame(
-      std::bind(&PointCloudPrivate::OnNewGpuRaysFrame, this,
+      std::bind(&PointCloudPrivate::OnNewDepthFrame, this,
       std::placeholders::_1, std::placeholders::_2, std::placeholders::_3,
       std::placeholders::_4, std::placeholders::_5));
 }
@@ -336,16 +324,27 @@ void PointCloudPrivate::OnNewDepthFrame(const float *_scan,
   if (this->pc_pub_.getNumSubscribers() <= 0 || _height == 0 || _width == 0)
     return;
 
-  // Just sanity checks, but don't prevent publishing
-  if (_channels != 1)
+  // Just sanity check, but don't prevent publishing
+  if (this->type_ == SensorType::RGBD_CAMERA && _channels != 1)
   {
     ROS_WARN_NAMED("ros1_ign_point_cloud",
         "Expected depth image to have 1 channel, but it has [%i]", _channels);
   }
-  if (_format != "FLOAT32")
+  if (this->type_ == SensorType::GPU_LIDAR && _channels != 3)
+  {
+    ROS_WARN_NAMED("ros1_ign_point_cloud",
+        "Expected GPU rays to have 3 channels, but it has [%i]", _channels);
+  }
+  if ((this->type_ == SensorType::RGBD_CAMERA ||
+       this->type_ == SensorType::DEPTH_CAMERA) && _format != "FLOAT32")
   {
     ROS_WARN_NAMED("ros1_ign_point_cloud",
         "Expected depth image to have [FLOAT32] format, but it has [%s]", _format.c_str());
+  }
+  if (this->type_ == SensorType::GPU_LIDAR && _format != "PF_FLOAT32_RGB")
+  {
+    ROS_WARN_NAMED("ros1_ign_point_cloud",
+        "Expected GPU rays to have [PF_FLOAT32_RGB] format, but it has [%s]", _format.c_str());
   }
 
   // Fill message
@@ -380,59 +379,94 @@ void PointCloudPrivate::OnNewDepthFrame(const float *_scan,
         _width, 3 * _width, this->rgb_image_.Data<unsigned char>());
   }
 
-  double hfov = this->depth_camera_->HFOV().Radian();
-  double fl = _width / (2.0 * tan(hfov / 2.0));
-  int index{0};
-  uint8_t * image_src = (uint8_t *)(&(this->rgb_image_msg_.data[0]));
+  // For depth calculation from image
+  double fl{0.0};
+  if (nullptr != this->depth_camera_)
+  {
+    auto hfov = this->depth_camera_->HFOV().Radian();
+    fl = _width / (2.0 * tan(hfov / 2.0));
+  }
 
-  // Convert depth to point cloud
+  // For depth calculation from laser scan
+  double angle_step{0.0};
+  double vertical_angle_step{0.0};
+  double inclination{0.0};
+  double azimuth{0.0};
+  if (nullptr != this->gpu_rays_)
+  {
+    angle_step = (this->gpu_rays_->AngleMax() - this->gpu_rays_->AngleMin()).Radian() /
+        (this->gpu_rays_->RangeCount()-1);
+    vertical_angle_step = (this->gpu_rays_->VerticalAngleMax() -
+        this->gpu_rays_->VerticalAngleMin()).Radian() / (this->gpu_rays_->VerticalRangeCount()-1);
+
+    // Angles of ray currently processing, azimuth is horizontal, inclination is vertical
+    inclination = this->gpu_rays_->VerticalAngleMin().Radian();
+    azimuth = this->gpu_rays_->AngleMin().Radian();
+  }
+
+  // For color calculation
+  uint8_t * image_src;
+  if (nullptr != this->rgb_camera_)
+  {
+    image_src = (uint8_t *)(&(this->rgb_image_msg_.data[0]));
+  }
+
+  // Iterate over scan and populate point cloud
   for (uint32_t j = 0; j < _height; ++j)
   {
-    double p_angle;
-    if (_height>1)
-      p_angle = atan2( (double)j - 0.5*(double)(_height-1), fl);
-    else
-      p_angle = 0.0;
+    double p_angle{0.0};
+    if (fl > 0 && _height > 1)
+      p_angle = atan2((double)j - 0.5 * (double)(_height-1), fl);
 
-    for (uint32_t i=0; i<_width; i++, ++iter_x, ++iter_y, ++iter_z, ++iter_r, ++iter_g, ++iter_b)
+    for (uint32_t i = 0; i < _width; ++i, ++iter_x, ++iter_y, ++iter_z, ++iter_r, ++iter_g, ++iter_b)
     {
-      double y_angle;
-      if (_width>1)
-        y_angle = atan2( (double)i - 0.5*(double)(_width-1), fl);
-      else
-        y_angle = 0.0;
+      // Index of current point
+      auto index = j * _width * _channels + i * _channels;
 
-      double depth = _scan[index++];
+      double y_angle{0.0};
+      if (fl > 0 && _width > 1)
+        y_angle = atan2((double)i - 0.5 * (double)(_width-1), fl);
 
-      // in optical frame
-      // hardcoded rotation rpy(-M_PI/2, 0, -M_PI/2) is built-in
-      // to urdf, where the *_optical_frame should have above relative
-      // rotation from the physical camera *_frame
-      *iter_x = depth * tan(y_angle);
-      *iter_y = depth * tan(p_angle);
-      if (depth > this->depth_camera_->FarClipPlane())
+      double depth = _scan[index];
+
+      if (nullptr != this->depth_camera_)
       {
-        *iter_z = ignition::math::INF_D;
-        msg.is_dense = false;
-      }
-      if (depth < this->depth_camera_->NearClipPlane())
-      {
-        *iter_z = -ignition::math::INF_D;
-        msg.is_dense = false;
-      }
-      else
-      {
+        // in optical frame
+        // hardcoded rotation rpy(-M_PI/2, 0, -M_PI/2) is built-in
+        // to urdf, where the *_optical_frame should have above relative
+        // rotation from the physical camera *_frame
+        *iter_x = depth * tan(y_angle);
+        *iter_y = depth * tan(p_angle);
         *iter_z = depth;
+
+        // Clamp according to REP 117
+        if (depth > this->depth_camera_->FarClipPlane())
+        {
+          *iter_z = ignition::math::INF_D;
+          msg.is_dense = false;
+        }
+        if (depth < this->depth_camera_->NearClipPlane())
+        {
+          *iter_z = -ignition::math::INF_D;
+          msg.is_dense = false;
+        }
+      }
+      else if (nullptr != this->gpu_rays_)
+      {
+        // Convert spherical coordinates to Cartesian for pointcloud
+        // See https://en.wikipedia.org/wiki/Spherical_coordinate_system
+        *iter_x = depth * cos(inclination) * cos(azimuth);
+        *iter_y = depth * cos(inclination) * sin(azimuth);
+        *iter_z = depth * sin(inclination);
       }
 
       // Put image color data for each point
-      // \TODO(anyone) RGB image seems offset from depth image by 3~4 pixels in both directions
       if (this->rgb_image_msg_.data.size() == _height * _width * 3)
       {
         // color
-        *iter_r = image_src[i*3+j*_width*3+0];
-        *iter_g = image_src[i*3+j*_width*3+1];
-        *iter_b = image_src[i*3+j*_width*3+2];
+        *iter_r = image_src[index+0];
+        *iter_g = image_src[index+1];
+        *iter_b = image_src[index+2];
       }
       else if (this->rgb_image_msg_.data.size() == _height*_width)
       {
@@ -448,60 +482,9 @@ void PointCloudPrivate::OnNewDepthFrame(const float *_scan,
         *iter_g = 0;
         *iter_b = 0;
       }
+      azimuth += angle_step;
     }
-  }
-
-  this->pc_pub_.publish(msg);
-}
-
-//////////////////////////////////////////////////
-void PointCloudPrivate::OnNewGpuRaysFrame(const float *_frame,
-                    unsigned int _width, unsigned int _height,
-                    unsigned int _depth,
-                    const std::string &_format)
-{
-  if (this->pc_pub_.getNumSubscribers() <= 0 || _height == 0 || _width == 0)
-    return;
-
-  if (_format != "PF_FLOAT32_RGB")
-  {
-    ROS_WARN_NAMED("ros1_ign_point_cloud",
-        "Expected depth image to have [FLOAT32] format, but it has [%s]", _format.c_str());
-  }
-
-  // Fill message
-  // Logic borrowed from
-  // https://github.com/ros-simulation/gazebo_ros_pkgs/blob/kinetic-devel/gazebo_plugins/src/gazebo_ros_depth_camera.cpp
-  auto sec_nsec = ignition::math::durationToSecNsec(this->current_time_);
-
-  sensor_msgs::PointCloud2 msg;
-  msg.header.frame_id = this->frame_id_;
-  msg.header.stamp.sec = sec_nsec.first;
-  msg.header.stamp.nsec = sec_nsec.second;
-  msg.width = _width;
-  msg.height = _height;
-  msg.row_step = msg.point_step * _width;
-  msg.is_dense = true;
-
-  sensor_msgs::PointCloud2Modifier modifier(msg);
-  modifier.setPointCloud2FieldsByString(2, "xyz", "rgb");
-  modifier.resize(_width*_height);
-
-  sensor_msgs::PointCloud2Iterator<float> iter_x(msg, "x");
-  sensor_msgs::PointCloud2Iterator<float> iter_y(msg, "y");
-  sensor_msgs::PointCloud2Iterator<float> iter_z(msg, "z");
-  sensor_msgs::PointCloud2Iterator<float> iter_rgb(msg, "rgb");
-
-  for (unsigned int i = 0; i < _width; ++i)
-  {
-    for (unsigned int j = 0; j < _height; ++j, ++iter_x, ++iter_y, ++iter_z, ++iter_rgb)
-    {
-      unsigned int index = (j * _width) + i;
-      *iter_x = _frame[4 * index];
-      *iter_y = _frame[4 * index + 1];
-      *iter_z = _frame[4 * index + 2];
-      *iter_rgb = _frame[4 * index + 3];
-    }
+    inclination += vertical_angle_step;
   }
 
   this->pc_pub_.publish(msg);
