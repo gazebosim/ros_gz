07088be4710cd1b02a2a05a71102dafda79e8b0b
Louise Poubel <louise@openrobotics.org>
2019-08-01 16:50:28 -0700

    tests for image

---
+++ ros1_ign_image/test/publishers/ign_publisher.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <atomic>
+#include <chrono>
+#include <csignal>
+#include <iostream>
+#include <thread>
+#include <ignition/msgs.hh>
+#include <ignition/transport.hh>
+#include "../test_utils.h"
+
+/// \brief Flag used to break the publisher loop and terminate the program.
+static std::atomic<bool> g_terminatePub(false);
+
+//////////////////////////////////////////////////
+/// \brief Function callback executed when a SIGINT or SIGTERM signals are
+/// captured. This is used to break the infinite loop that publishes messages
+/// and exit the program smoothly.
+void signal_handler(int _signal)
+{
+  if (_signal == SIGINT || _signal == SIGTERM)
+    g_terminatePub = true;
+}
+
+//////////////////////////////////////////////////
+int main(int /*argc*/, char **/*argv*/)
+{
+  // Install a signal handler for SIGINT and SIGTERM.
+  std::signal(SIGINT,  signal_handler);
+  std::signal(SIGTERM, signal_handler);
+
+  // Create a transport node and advertise a topic.
+  ignition::transport::Node node;
+
+  // ignition::msgs::Image.
+  auto image_pub = node.Advertise<ignition::msgs::Image>("image");
+  ignition::msgs::Image image_msg;
+  ros1_ign_image::testing::createTestMsg(image_msg);
+
+  // Publish messages at 1Hz.
+  while (!g_terminatePub)
+  {
+    image_pub.Publish(image_msg);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  return 0;
+}
