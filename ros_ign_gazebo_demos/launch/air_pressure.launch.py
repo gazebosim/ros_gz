ee88cdd36a4124e0a039dbfcf32e2e3f1c369c41
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-12-11 23:32:45 +0100

    [WIP] Port ign_ros_gazebo_demos to ROS2 (#58)
    Port ros_ign_image to ROS2
    Port ros_ign_gazebo_demos to ROS2
    
    * Some demos are blocked by ros_ign_point_cloud
    * Some demos are pending RViz displays
    * air_pressure is missing FluidPressure message bridge

---
+++ ros_ign_gazebo_demos/launch/air_pressure.launch.py
@@ -0,0 +1,72 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Launch Ignition Gazebo with command line arguments."""
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.actions import ExecuteProcess
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+# TODO; execution broken
+# Failed to create a bridge for topic [/air_pressure] with ROS2 type
+# [sensor_msgs/FluidPressure] and Ignition Transport type
+# [ignition.msgs.FluidPressure]
+
+def generate_launch_description():
+
+    pkg_ros_ign_gazebo_demos = get_package_share_directory('ros_ign_gazebo_demos')
+
+    # Ignition Gazebo
+    ign_gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_ign_gazebo_demos, 'launch', 'ign_gazebo.launch.py'),
+        )
+    )
+
+    # Bridge
+    # TODO: Needs sensor_msgs/msg/FluidPressure to be supported on bridge
+    # bridge = Node(
+    #     package='ros_ign_bridge',
+    #     node_executable='parameter_bridge',
+    #     arguments=["/air_pressure@sensor_msgs/msg/FluidPressure@ignition.msgs.FluidPressure"],
+    #     output='screen'
+    # )
+
+    # RQt
+    rqt = Node(
+        package='rqt_topic',
+        node_executable='rqt_topic',
+        arguments=['-t'],
+        condition=IfCondition(LaunchConfiguration('rqt'))
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+          'args',
+          default_value=['sensors.sdf'],
+          description='Ignition Gazebo arguments'),
+        DeclareLaunchArgument('rqt', default_value='true',
+                              description='Open RQt.'),
+        ign_gazebo,
+    #    bridge,
+        rqt
+    ])
---
+++ ros_ign_gazebo_demos/launch/battery.launch.py
@@ -0,0 +1,65 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    pkg_ros_ign_gazebo_demos = get_package_share_directory('ros_ign_gazebo_demos')
+
+    # Ignition Gazebo
+    ign_gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_ign_gazebo_demos, 'launch', 'ign_gazebo.launch.py'),
+        )
+    )
+
+    # RQt
+    rqt = Node(
+        package='rqt_plot',
+        node_executable='rqt_plot',
+        # FIXME: Why isn't the topic being populated on the UI? RQt issue?
+        arguments=['--force-discover', '/model/vehicle_blue/battery/linear_battery/state/percentage'],
+        condition=IfCondition(LaunchConfiguration('rqt'))
+    )
+
+    # Bridge
+    bridge = Node(
+        package='ros_ign_bridge',
+        node_executable='parameter_bridge',
+        arguments=['/model/vehicle_blue/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist',
+                   '/model/vehicle_blue/battery/linear_battery/state@sensor_msgs/msg/BatteryState@ignition.msgs.BatteryState'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+          'args',
+          default_value=['-r -z 1000000 linear_battery_demo.sdf'],
+          description='Ignition Gazebo arguments'),
+        DeclareLaunchArgument('rqt', default_value='true',
+                              description='Open RQt.'),
+        ign_gazebo,
+        bridge,
+        rqt
+    ])
