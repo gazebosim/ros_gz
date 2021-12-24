11a1cfd275526d318774e72fceaabd5fc4fe2ba0
Louise Poubel <louise@openrobotics.org>
2021-12-23 18:11:08 -0800

    More updates for Fortress
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -10,27 +10,7 @@ export ROS_PYTHON_VERSION=3
 apt update -qq
 apt install -qq -y lsb-release wget curl build-essential
 
-# Citadel gets Ignition with rosdep
-if [ "$IGNITION_VERSION" != "citadel" ]; then
-  echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list
-  echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-prerelease `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-prerelease.list
-  echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-nightly `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-nightly.list
-  wget https://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
-
-  if [ "$IGNITION_VERSION" == "dome" ]; then
-    IGN_DEPS="libignition-msgs6-dev libignition-transport9-dev libignition-gazebo4-dev"
-  fi
-
-  if [ "$IGNITION_VERSION" == "edifice" ]; then
-    IGN_DEPS="libignition-msgs7-dev libignition-transport10-dev libignition-gazebo5-dev"
-  fi
-
-  if [ "$IGNITION_VERSION" == "fortress" ]; then
-    IGN_DEPS="libignition-msgs8-dev libignition-transport11-dev libignition-gazebo6-dev"
-  fi
-fi
-
-# Dependencies.
+# Dependencies, including Ignition
 echo "deb http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 apt-get update -qq
