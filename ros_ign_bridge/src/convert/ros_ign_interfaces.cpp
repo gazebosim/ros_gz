bdb05fe6d70869d647f6f7ab2174576905f0ea9b
Carlos Agüero <caguero@openrobotics.org>
2022-05-23 20:40:50 +0200

    Add rssi to Dataframe.msg (#249)
    * Adding rssi field to ros_ign_interfaces/Dataframe.msg
    
    Signed-off-by: Carlos Agüero <caguero@openrobotics.org>

--- ros_ign_bridge/src/convert/ros_ign_interfaces.cpp
+++ ros_ign_bridge/src/convert/ros_ign_interfaces.cpp
@@ -209,6 +209,9 @@ convert_ros_to_ign(
   ignition::msgs::Dataframe & ign_msg)
 {
   convert_ros_to_ign(ros_msg.header, (*ign_msg.mutable_header()));
+  auto * rssiPtr = ign_msg.mutable_header()->add_data();
+  rssiPtr->set_key("rssi");
+  rssiPtr->add_value(std::to_string(ros_msg.rssi));
 
   ign_msg.set_src_address(ros_msg.src_address);
   ign_msg.set_dst_address(ros_msg.dst_address);
@@ -226,6 +229,21 @@ convert_ign_to_ros(
 
   ros_msg.src_address = ign_msg.src_address();
   ros_msg.dst_address = ign_msg.dst_address();
+
+  const auto & header = ign_msg.header();
+  for (auto i = 0; i < header.data_size(); ++i) {
+    if (header.data(i).key() == "rssi" && header.data(i).value_size() > 0) {
+      try {
+        ros_msg.rssi = std::stod(header.data(i).value(0));
+      } catch (const std::invalid_argument &) {
+        std::cerr << "RSSI value is invalid (" <<
+          header.data(i).value(0) << ")" << std::endl;
+      } catch (const std::out_of_range &) {
+        std::cerr << "RSSI value is out of range (" <<
+          header.data(i).value(0) << ")" << std::endl;
+      }
+    }
+  }
 
   ros_msg.data.resize(ign_msg.data().size());
   std::copy(
