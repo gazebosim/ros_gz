ef5ca5437f929adddf78bc31ce0b5bcd0b20e0f0
methylDragon <methylDragon@gmail.com>
2022-08-01 13:24:18 -0700

    Implement shims
    Signed-off-by: methylDragon <methylDragon@gmail.com>

---
+++ ros_gz_shims/ros_ign_bridge/src/static_bridge_shim.cpp
@@ -0,0 +1,30 @@
+// Shim to redirect "ros_ign_bridge static_bridge" call to "ros_gz_bridge static_bridge"
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <stdlib.h>
+
+#include <ament_index_cpp/get_package_prefix.hpp>
+
+
+int main(int argc, char * argv[])
+{
+  std::stringstream cli_call;
+
+  cli_call << ament_index_cpp::get_package_prefix("ros_gz_bridge")
+           << "/lib/ros_gz_bridge/static_bridge";
+
+  if (argc > 1)
+  {
+    for (int i = 1; i < argc; i++)
+      cli_call << " " << argv[i];
+  }
+
+  std::cerr << "[ros_ign_bridge] is deprecated! "
+            << "Redirecting to use [ros_gz_bridge] instead!"
+            << std::endl << std::endl;
+  system(cli_call.str().c_str());
+
+  return 0;
+}
---
+++ ros_gz_shims/ros_ign_gazebo/src/create_shim.cpp
@@ -0,0 +1,30 @@
+// Shim to redirect "ros_ign_bridge parameter_bridge" call to "ros_gz_sim parameter_bridge"
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <stdlib.h>
+
+#include <ament_index_cpp/get_package_prefix.hpp>
+
+
+int main(int argc, char * argv[])
+{
+  std::stringstream cli_call;
+
+  cli_call << ament_index_cpp::get_package_prefix("ros_gz_sim")
+           << "/lib/ros_gz_sim/create";
+
+  if (argc > 1)
+  {
+    for (int i = 1; i < argc; i++)
+      cli_call << " " << argv[i];
+  }
+
+  std::cerr << "[ros_ign_gazebo] is deprecated! "
+            << "Redirecting to use [ros_gz_sim] instead!"
+            << std::endl << std::endl;
+  system(cli_call.str().c_str());
+
+  return 0;
+}
