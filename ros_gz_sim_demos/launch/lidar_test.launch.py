3fac23f2981a92d818ebb5a59b8c0152d0c020f7
Connor <connor.taylor222@gmail.com>
2022-11-03 11:24:46 -0300

    adding lidar_test launch file to test the lidar sensor update, also added necessary models/worlds
    Signed-off-by: Connor <connor.taylor222@gmail.com>

---
+++ ros_gz_sim_demos/launch/lidar_test.launch.py
@@ -0,0 +1,60 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={'gz_args': '-v4 -r lidar_test.sdf' }.items(),
+    )
+
+    # RViz
+    rviz = Node(
+       package='rviz2',
+       executable='rviz2',
+       # FIXME: Generate new RViz config once this demo is usable again
+       # arguments=['-d', os.path.join(pkg_ros_gz_sim_demos, 'rviz', 'gpu_lidar.rviz')],
+       condition=IfCondition(LaunchConfiguration('rviz'))
+    )
+
+    # Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/lidar@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan'],
+        output='screen'
+    )
+
+    # FIXME: need a SDF file (gpu_lidar.sdf) inside ros_gz_point_cloud/
+    return LaunchDescription([
+        gz_sim,
+        bridge,
+    ])
