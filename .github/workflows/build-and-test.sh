cc70efdeb8a2ea425a97e571ef1e8b1cd71e95b5
Michael Carroll <mjcarroll@intrinsic.ai>
2023-08-28 22:37:38 +0000

    Add harmonic stanza
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -16,7 +16,15 @@ if [ "$GZ_VERSION" == "garden" ]; then
   echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-nightly `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-nightly.list
   wget https://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
 
-  GZ_DEPS="libgz-sim7-dev libgz-sim8-dev"
+  GZ_DEPS="libgz-sim7-dev"
+
+  ROSDEP_ARGS="--skip-keys='sdformat-urdf'"
+elif [ "$GZ_VERSION" == "harmonic" ]; then
+  echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list
+  echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-nightly `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-nightly.list
+  wget https://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
+
+  GZ_DEPS="libgz-sim8-dev"
 
   ROSDEP_ARGS="--skip-keys='sdformat-urdf'"
 fi
