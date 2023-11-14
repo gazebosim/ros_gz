682dd66f2b2e00aa7d234e678922793d8c477632
Addisu Z. Taddese <addisu@openrobotics.org>
2023-11-14 12:42:05 -0600

    Add additional cmake/package.xml changes
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -19,7 +19,6 @@ if [ "$GZ_VERSION" == "garden" ]; then
   ROSDEP_ARGS="--skip-keys='sdformat-urdf'"
 elif [ "$GZ_VERSION" == "harmonic" ]; then
   echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list
-  echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-prerelease `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-prerelease.list
   wget https://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
 
   GZ_DEPS="libgz-sim8-dev"
