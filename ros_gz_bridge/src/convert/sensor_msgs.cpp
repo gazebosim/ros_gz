d36e3717429f71d72cccaaae041fe074bbc33440
El Jawad Alaa <ejalaa12@gmail.com>
2024-01-23 14:39:14 +0100

    populate imu covariances when converting (#488)
    Signed-off-by: Alaa El Jawad <ejalaa12@gmail.com>

--- ros_gz_bridge/src/convert/sensor_msgs.cpp
+++ ros_gz_bridge/src/convert/sensor_msgs.cpp
@@ -17,6 +17,12 @@
 #include "convert/utils.hpp"
 #include "ros_gz_bridge/convert/sensor_msgs.hpp"
 
+#include "gz/msgs/config.hh"
+
+#if GZ_MSGS_MAJOR_VERSION >= 10
+#define GZ_MSGS_IMU_HAS_COVARIANCE
+#endif
+
 namespace ros_gz_bridge
 {
 
@@ -274,6 +280,18 @@ convert_ros_to_gz(
   convert_ros_to_gz(ros_msg.orientation, (*gz_msg.mutable_orientation()));
   convert_ros_to_gz(ros_msg.angular_velocity, (*gz_msg.mutable_angular_velocity()));
   convert_ros_to_gz(ros_msg.linear_acceleration, (*gz_msg.mutable_linear_acceleration()));
+
+#ifdef GZ_MSGS_IMU_HAS_COVARIANCE
+  for (const auto & elem : ros_msg.linear_acceleration_covariance) {
+    gz_msg.mutable_linear_acceleration_covariance()->add_data(elem);
+  }
+  for (const auto & elem : ros_msg.orientation_covariance) {
+    gz_msg.mutable_orientation_covariance()->add_data(elem);
+  }
+  for (const auto & elem : ros_msg.angular_velocity_covariance) {
+    gz_msg.mutable_angular_velocity_covariance()->add_data(elem);
+  }
+#endif
 }
 
 template<>
@@ -287,7 +305,29 @@ convert_gz_to_ros(
   convert_gz_to_ros(gz_msg.angular_velocity(), ros_msg.angular_velocity);
   convert_gz_to_ros(gz_msg.linear_acceleration(), ros_msg.linear_acceleration);
 
-  // Covariances not supported in gz::msgs::IMU
+#ifdef GZ_MSGS_IMU_HAS_COVARIANCE
+  int data_size = gz_msg.linear_acceleration_covariance().data_size();
+  if (data_size == 9) {
+    for (int i = 0; i < data_size; ++i) {
+      auto data = gz_msg.linear_acceleration_covariance().data()[i];
+      ros_msg.linear_acceleration_covariance[i] = data;
+    }
+  }
+  data_size = gz_msg.angular_velocity_covariance().data_size();
+  if (data_size == 9) {
+    for (int i = 0; i < data_size; ++i) {
+      auto data = gz_msg.angular_velocity_covariance().data()[i];
+      ros_msg.angular_velocity_covariance[i] = data;
+    }
+  }
+  data_size = gz_msg.orientation_covariance().data_size();
+  if (data_size == 9) {
+    for (int i = 0; i < data_size; ++i) {
+      auto data = gz_msg.orientation_covariance().data()[i];
+      ros_msg.orientation_covariance[i] = data;
+    }
+  }
+#endif
 }
 
 template<>
