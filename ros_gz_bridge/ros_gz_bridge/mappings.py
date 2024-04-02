409a94107912ba9f3808c791c1c7c271ae76877e
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-04-02 09:48:58 +0200

    Added conversion for Detection3D and Detection3DArray (#523) (#526)
    Signed-off-by: wittenator <9154515+wittenator@users.noreply.github.com>
    Co-authored-by: wittenator <9154515+wittenator@users.noreply.github.com>

--- ros_gz_bridge/ros_gz_bridge/mappings.py
+++ ros_gz_bridge/ros_gz_bridge/mappings.py
@@ -106,6 +106,8 @@ MAPPINGS = {
     'vision_msgs': [
         Mapping('Detection2DArray', 'AnnotatedAxisAligned2DBox_V'),
         Mapping('Detection2D', 'AnnotatedAxisAligned2DBox'),
+        Mapping('Detection3DArray', 'AnnotatedOriented3DBox_V'),
+        Mapping('Detection3D', 'AnnotatedOriented3DBox'),
     ],
 }
 
