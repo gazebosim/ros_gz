d87d63db8f3cf61297bcc4cc84829586532bfd13
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-04-16 15:00:27 +0200

    [backport Iron] Create bridge for GPSFix msg (#316) (#537)
    Signed-off-by: Vincent Rousseau <vincent.rousseau@sabi-agri.com>
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    Co-authored-by: Rousseau Vincent <vincentrou@gmail.com>

---
+++ ros_gz_sim_demos/launch/navsat_gpsfix.launch.py
@@ -0,0 +1,63 @@
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={
+            'gz_args': '-v 4 -r spherical_coordinates.sdf'
+        }.items(),
+    )
+
+    # RQt
+    rqt = Node(
+        package='rqt_topic',
+        executable='rqt_topic',
+        arguments=['-t'],
+        condition=IfCondition(LaunchConfiguration('rqt'))
+    )
+
+    # Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/navsat@gps_msgs/msg/GPSFix@gz.msgs.NavSat'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gz_sim,
+        DeclareLaunchArgument('rqt', default_value='true',
+                              description='Open RQt.'),
+        bridge,
+        rqt
+    ])
