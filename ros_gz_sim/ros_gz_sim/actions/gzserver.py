2416aafce0bd0b4e3ff2f1ca3ecfdb3e4d2813cf
Carlos Agüero <caguero@openrobotics.org>
2024-05-23 22:21:11 +0200

    Mostly styule.
    Signed-off-by: Carlos Agüero <caguero@openrobotics.org>

--- ros_gz_sim/ros_gz_sim/actions/gzserver.py
+++ ros_gz_sim/ros_gz_sim/actions/gzserver.py
@@ -19,97 +19,93 @@ from typing import Optional
 
 from launch.action import Action
 from launch.actions import IncludeLaunchDescription
-from launch.frontend import expose_action, Entity, Parser
+from launch.frontend import Entity, expose_action, Parser
 from launch.launch_context import LaunchContext
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.some_substitutions_type import SomeSubstitutionsType
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
+
 @expose_action('gz_server')
 class GzServer(Action):
-	"""Action that executes a gz_server ROS [composable] node."""
+    """Action that executes a gz_server ROS [composable] node."""
 
-	def __init__(
-		self,
-		*,
-		world_sdf_file: Optional[SomeSubstitutionsType],
-		world_sdf_string: Optional[SomeSubstitutionsType],
-		container_name: Optional[SomeSubstitutionsType],
-		use_composition: Optional[SomeSubstitutionsType],
-		**kwargs
-	) -> None:
-		"""
-		Construct a gz_server action.
+    def __init__(
+        self,
+        *,
+        world_sdf_file: Optional[SomeSubstitutionsType] = None,
+        world_sdf_string: Optional[SomeSubstitutionsType] = None,
+        container_name: Optional[SomeSubstitutionsType] = None,
+        use_composition: Optional[SomeSubstitutionsType] = None,
+        **kwargs
+    ) -> None:
+        """
+        Construct a gz_server action.
 
-		All arguments are forwarded to `ros_gz_sim.launch.gz_server.launch.py`, so see the documentation
-		of that class for further details.
+        All arguments are forwarded to `ros_gz_sim.launch.gz_server.launch.py`,
+        so see the documentation of that class for further details.
 
-		:param: world_sdf_file Path to the SDF world file.
-		:param: world_sdf_string SDF world string.
-		:param: container_name Name of container that nodes will load in if use composition.
-		:param: use_composition Use composed bringup if True.
-		"""
+        :param: world_sdf_file Path to the SDF world file.
+        :param: world_sdf_string SDF world string.
+        :param: container_name Name of container that nodes will load in if use composition.
+        :param: use_composition Use composed bringup if True.
+        """
+        super().__init__(**kwargs)
+        self.__world_sdf_file = world_sdf_file
+        self.__world_sdf_string = world_sdf_string
+        self.__container_name = container_name
+        self.__use_composition = use_composition
 
-		super().__init__(**kwargs)
-		self.__world_sdf_file = world_sdf_file
-		self.__world_sdf_string = world_sdf_string
-		self.__container_name = container_name
-		self.__use_composition = use_composition
+    @classmethod
+    def parse(cls, entity: Entity, parser: Parser):
+        """Parse gz_server."""
+        _, kwargs = super().parse(entity, parser)
 
-	@classmethod
-	def parse(cls, entity: Entity, parser: Parser):
-		"""Parse gz_server."""
-		_, kwargs = super().parse(entity, parser)
+        world_sdf_file = entity.get_attr(
+            'world_sdf_file', data_type=str,
+            optional=True)
 
-		world_sdf_file = entity.get_attr(
-			'world_sdf_file', data_type=str,
-			optional=True)
+        world_sdf_string = entity.get_attr(
+            'world_sdf_string', data_type=str,
+            optional=True)
 
-		world_sdf_string = entity.get_attr(
-		    'world_sdf_string', data_type=str,
-		    optional=True)
+        container_name = entity.get_attr(
+            'container_name', data_type=str,
+            optional=True)
 
-		container_name = entity.get_attr(
-		    'container_name', data_type=str,
-		    optional=True)
+        use_composition = entity.get_attr(
+            'use_composition', data_type=str,
+            optional=True)
 
-		use_composition = entity.get_attr(
-		    'use_composition', data_type=str,
-		    optional=True)
+        if isinstance(world_sdf_file, str):
+            world_sdf_file = parser.parse_substitution(world_sdf_file)
+            kwargs['world_sdf_file'] = world_sdf_file
 
-		if isinstance(world_sdf_file, str):
-			world_sdf_file = parser.parse_substitution(world_sdf_file)
-			kwargs['world_sdf_file'] = world_sdf_file
+        if isinstance(world_sdf_string, str):
+            world_sdf_string = parser.parse_substitution(world_sdf_string)
+            kwargs['world_sdf_string'] = world_sdf_string
 
-		if isinstance(world_sdf_string, str):
-		    world_sdf_string = parser.parse_substitution(world_sdf_string)
-		    kwargs['world_sdf_string'] = world_sdf_string
+        if isinstance(container_name, str):
+            container_name = parser.parse_substitution(container_name)
+            kwargs['container_name'] = container_name
 
-		if isinstance(container_name, str):
-		    container_name = parser.parse_substitution(container_name)
-		    kwargs['container_name'] = container_name
+        if isinstance(use_composition, str):
+            use_composition = parser.parse_substitution(use_composition)
+            kwargs['use_composition'] = use_composition
 
-		if isinstance(use_composition, str):
-		    use_composition = parser.parse_substitution(use_composition)
-		    kwargs['use_composition'] = use_composition
+        return cls, kwargs
 
-		return cls, kwargs
+    def execute(self, context: LaunchContext) -> Optional[List[Action]]:
+        """Execute the action."""
+        gz_server_description = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
+                                       'launch',
+                                       'gz_server.launch.py'])]),
+            launch_arguments=[('world_sdf_file', self.__world_sdf_file),
+                              ('world_sdf_string', self.__world_sdf_string),
+                              ('container_name',   self.__container_name),
+                              ('use_composition',  self.__use_composition), ])
 
-	def execute(self, context: LaunchContext) -> Optional[List[Action]]:
-		"""
-		Execute the action.
-		"""
-
-		gz_server_description = IncludeLaunchDescription(
-			PythonLaunchDescriptionSource(
-				[PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
-									   'launch',
-									   'gz_server.launch.py'])]),
-				launch_arguments=[('world_sdf_file', self.__world_sdf_file),
-								  ('world_sdf_string', self.__world_sdf_string),
-								  ('container_name',   self.__container_name),
-								  ('use_composition',  self.__use_composition),
-								 ])
-
-		return [gz_server_description]
+        return [gz_server_description]
