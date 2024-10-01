f0f63b3d9182506687cd88a14bf93ea726afaec6
Carlos Agüero <caguero@openrobotics.org>
2024-10-01 19:39:06 +0200

    Use create_own_container
    Signed-off-by: Carlos Agüero <caguero@openrobotics.org>

--- ros_gz_bridge/launch/ros_gz_bridge.launch.py
+++ ros_gz_bridge/launch/ros_gz_bridge.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
     bridge_name = LaunchConfiguration('bridge_name')
     config_file = LaunchConfiguration('config_file')
     container_name = LaunchConfiguration('container_name')
-    start_container = LaunchConfiguration('start_container')
+    create_own_container = LaunchConfiguration('create_own_container')
     namespace = LaunchConfiguration('namespace')
     use_composition = LaunchConfiguration('use_composition')
     use_respawn = LaunchConfiguration('use_respawn')
@@ -47,8 +47,8 @@ def generate_launch_description():
         description='Name of container that nodes will load in if use composition',
     )
 
-    declare_start_container_cmd = DeclareLaunchArgument(
-        'start_container',
+    declare_create_own_container_cmd = DeclareLaunchArgument(
+        'create_own_container',
         default_value='False',
         description='Whether to start a ROS container when using composition',
     )
@@ -58,7 +58,7 @@ def generate_launch_description():
     )
 
     declare_use_composition_cmd = DeclareLaunchArgument(
-        'use_composition', default_value='True', description='Use composed bringup if True'
+        'use_composition', default_value='False', description='Use composed bringup if True'
     )
 
     declare_use_respawn_cmd = DeclareLaunchArgument(
@@ -89,7 +89,7 @@ def generate_launch_description():
     )
 
     load_composable_nodes_with_container = ComposableNodeContainer(
-        condition=IfCondition(PythonExpression([use_composition, ' and ', start_container])),
+        condition=IfCondition(PythonExpression([use_composition, ' and ', create_own_container])),
         name=LaunchConfiguration('container_name'),
         namespace='',
         package='rclcpp_components',
@@ -108,7 +108,7 @@ def generate_launch_description():
     )
 
     load_composable_nodes_without_container = LoadComposableNodes(
-        condition=IfCondition(PythonExpression([use_composition, ' and not ', start_container])),
+        condition=IfCondition(PythonExpression([use_composition, ' and not ', create_own_container])),
         target_container=container_name,
         composable_node_descriptions=[
             ComposableNode(
@@ -130,7 +130,7 @@ def generate_launch_description():
     ld.add_action(declare_bridge_name_cmd)
     ld.add_action(declare_config_file_cmd)
     ld.add_action(declare_container_name_cmd)
-    ld.add_action(declare_start_container_cmd)
+    ld.add_action(declare_create_own_container_cmd)
     ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_composition_cmd)
     ld.add_action(declare_use_respawn_cmd)
