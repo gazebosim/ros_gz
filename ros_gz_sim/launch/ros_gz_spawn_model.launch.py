082a7625208eeb9a55cc373ba70084c425da2d95
Aarav Gupta <134804732+Amronos@users.noreply.github.com>
2024-10-18 12:46:03 +0000

    Add create_own_container argument to ros_gz_spawn_model.launch.py
    Signed-off-by: Aarav Gupta <134804732+Amronos@users.noreply.github.com>

--- ros_gz_sim/launch/ros_gz_spawn_model.launch.py
+++ ros_gz_sim/launch/ros_gz_spawn_model.launch.py
@@ -26,6 +26,7 @@ def generate_launch_description():
     bridge_name = LaunchConfiguration('bridge_name')
     config_file = LaunchConfiguration('config_file')
     container_name = LaunchConfiguration('container_name')
+    create_own_container = LaunchConfiguration('create_own_container')
     namespace = LaunchConfiguration('namespace')
     use_composition = LaunchConfiguration('use_composition')
     use_respawn = LaunchConfiguration('use_respawn')
@@ -58,12 +59,18 @@ def generate_launch_description():
         description='Name of container that nodes will load in if use composition',
     )
 
+    declare_create_own_container_cmd = DeclareLaunchArgument(
+        'create_own_container',
+        default_value='False',
+        description='Whether we should start a ROS container when using composition.',
+    )
+
     declare_namespace_cmd = DeclareLaunchArgument(
         'namespace', default_value='', description='Top-level namespace'
     )
 
     declare_use_composition_cmd = DeclareLaunchArgument(
-        'use_composition', default_value='True', description='Use composed bringup if True'
+        'use_composition', default_value='False', description='Use composed bringup if True'
     )
 
     declare_use_respawn_cmd = DeclareLaunchArgument(
@@ -113,6 +120,7 @@ def generate_launch_description():
         launch_arguments=[('bridge_name', bridge_name),
                           ('config_file', config_file),
                           ('container_name', container_name),
+                          ('create_own_container', create_own_container),
                           ('namespace', namespace),
                           ('use_composition', use_composition),
                           ('use_respawn', use_respawn),
@@ -143,6 +151,7 @@ def generate_launch_description():
     ld.add_action(declare_bridge_name_cmd)
     ld.add_action(declare_config_file_cmd)
     ld.add_action(declare_container_name_cmd)
+    ld.add_action(declare_create_own_container_cmd)
     ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_composition_cmd)
     ld.add_action(declare_use_respawn_cmd)
