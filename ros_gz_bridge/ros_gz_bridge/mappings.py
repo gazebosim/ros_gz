9222e2d8960467bfeb321bbdb9847e2cdb5ac2d9
chcaya <55998117+chcaya@users.noreply.github.com>
2025-05-06 04:56:45 -0400

    Added LaserScan-Range bridge (#736)
    Signed-off-by: chcaya <cayc2401@usherbrooke.ca>

--- ros_gz_bridge/ros_gz_bridge/mappings.py
+++ ros_gz_bridge/ros_gz_bridge/mappings.py
@@ -90,6 +90,7 @@ MAPPINGS = {
         Mapping('MagneticField', 'Magnetometer'),
         Mapping('NavSatFix', 'NavSat'),
         Mapping('PointCloud2', 'PointCloudPacked'),
+        Mapping('Range', 'LaserScan'),
     ],
     'std_msgs': [
         Mapping('Bool', 'Boolean'),
