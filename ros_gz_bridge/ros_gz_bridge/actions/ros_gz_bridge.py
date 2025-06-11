e2a21c330a406653718cface14d99e8f7ca68d4b
Martin Pecka <peckama2@fel.cvut.cz>
2025-06-11 10:42:11 +0200

    feat(ros_gz_bridge)!: Allow setting QoS profile from YAML files and launch action. (#761)
    This change breaks API and ABI of the BridgeConfig struct.
    
    Signed-off-by: Martin Pecka <peckama2@fel.cvut.cz>

--- ros_gz_bridge/ros_gz_bridge/actions/ros_gz_bridge.py
+++ ros_gz_bridge/ros_gz_bridge/actions/ros_gz_bridge.py
@@ -197,6 +197,7 @@ class RosGzBridge(Action):
                 ('lazy', str),
                 ('publisher_queue', int),
                 ('subscriber_queue', int),
+                ('qos_profile', str),
                 ('frame_id', str),
             )
             for param, param_type in optional_params:
--- ros_gz_bridge/src/bridge_handle_gz_to_ros.cpp
+++ ros_gz_bridge/src/bridge_handle_gz_to_ros.cpp
@@ -67,7 +67,7 @@ void BridgeHandleGzToRos::StartPublisher()
   this->ros_publisher_ = this->factory_->create_ros_publisher(
     this->ros_node_,
     this->config_.ros_topic_name,
-    this->config_.publisher_queue_size);
+    this->config_.PublisherQoS());
 }
 
 bool BridgeHandleGzToRos::HasSubscriber() const
@@ -85,7 +85,7 @@ void BridgeHandleGzToRos::StartSubscriber()
   this->factory_->create_gz_subscriber(
     this->gz_node_,
     this->config_.gz_topic_name,
-    this->config_.subscriber_queue_size,
+    this->config_.subscriber_queue_size.value_or(kDefaultSubscriberQueue),
     this->ros_publisher_,
     this->gz_to_ros_parameters_);
 
