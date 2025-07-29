06ffa9bcac5bb03636ec9d7c5a7dda831be32719
Addisu Z. Taddese <addisu@openrobotics.org>
2025-07-29 17:50:29 -0500

    Include stable repos
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -10,6 +10,7 @@ export ROS_PYTHON_VERSION=3
 apt update -qq
 apt install -qq -y lsb-release wget curl build-essential
 
+echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list
 echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-nightly `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-nightly.list
 wget https://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
 
