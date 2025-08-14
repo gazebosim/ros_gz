7d1918ac1f6baa15f46efab06368e6daa49c3291
Addisu Z. Taddese <addisu@openrobotics.org>
2025-08-14 15:53:44 -0500

    Update to simulation_interfaces 2.0
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- ros_gz_sim/src/gz_simulation_interfaces/services/spawn_entity.cpp
+++ ros_gz_sim/src/gz_simulation_interfaces/services/spawn_entity.cpp
@@ -42,13 +42,14 @@ SpawnEntity::SpawnEntity(
         gz_request.set_name(request->name);
       }
       gz_request.set_allow_renaming(request->allow_renaming);
+      const auto &resource = request->entity_resource;
 
-      if (!request->uri.empty()) {
+      if (!resource.uri.empty()) {
         // TODO(azeey) The `sdf_filename` field requires absolute paths to the file.
         // Consider resolving the uri using the `/gazebo/resource_paths/resolve`
-        gz_request.set_sdf_filename(request->uri);
-      } else if (!request->resource_string.empty()) {
-        gz_request.set_sdf(request->resource_string);
+        gz_request.set_sdf_filename(resource.uri);
+      } else if (!resource.resource_string.empty()) {
+        gz_request.set_sdf(resource.resource_string);
       } else {
         response->result.result = Result::RESULT_OPERATION_FAILED;
         response->result.error_message =
