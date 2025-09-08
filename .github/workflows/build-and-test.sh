19b2e3486da994c3a96eaad0656f3de202f01590
Addisu Z. Taddese <addisu@openrobotics.org>
2025-09-08 10:14:34 -0500

    Remove packages.osrfoundation.org as we only need to depend on vendor packages
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -10,18 +10,12 @@ export ROS_PYTHON_VERSION=3
 apt update -qq
 apt install -qq -y lsb-release wget curl build-essential
 
-echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list
-echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-nightly `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-nightly.list
-wget https://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
-
 # Dependencies.
 echo "deb http://packages.ros.org/ros2-testing/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-testing.list
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 apt-get update -qq
 apt-get install -y python3-colcon-common-extensions \
-                   python3-rosdep \
-                   libcli11-dev \
-                   gz-jetty
+                   python3-rosdep
 
 rosdep init
 rosdep update
