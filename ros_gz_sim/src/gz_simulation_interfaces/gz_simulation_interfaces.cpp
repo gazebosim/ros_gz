821993e281f44baa333ba91913d7468e86bb6ef3
Addisu Z. Taddese <addisu@openrobotics.org>
2025-09-08 23:32:02 -0500

    fix uncrustify errors
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- ros_gz_sim/src/gz_simulation_interfaces/gz_simulation_interfaces.cpp
+++ ros_gz_sim/src/gz_simulation_interfaces/gz_simulation_interfaces.cpp
@@ -67,7 +67,7 @@ public:
   void UpdateStateFromMsg(const gz::msgs::SerializedStepMap & msg);
   void CreateInterfaces();
 
-  template <typename Handler>
+  template<typename Handler>
   void AddInterface();
 
   bool InitializeGazeboParameters();
@@ -87,12 +87,12 @@ GzSimulationInterfaces::Implementation::Implementation(std::shared_ptr<rclcpp::N
 void GzSimulationInterfaces::Implementation::Run()
 {
   auto thread = std::thread([&] {
-    try {
-      this->gz_proxy_ = std::make_shared<GazeboProxy>(this->world_name_, this->ros_node_);
-      this->CreateInterfaces();
-    } catch (const std::exception & e) {
-      RCLCPP_ERROR_STREAM(this->ros_node_->get_logger(), e.what());
-    }
+        try {
+          this->gz_proxy_ = std::make_shared<GazeboProxy>(this->world_name_, this->ros_node_);
+          this->CreateInterfaces();
+        } catch (const std::exception & e) {
+          RCLCPP_ERROR_STREAM(this->ros_node_->get_logger(), e.what());
+        }
   });
 
   thread.detach();
@@ -118,7 +118,7 @@ void GzSimulationInterfaces::Implementation::CreateInterfaces()
   this->AddInterface<actions::SimulateSteps>();
 }
 
-template <typename Interface>
+template<typename Interface>
 void GzSimulationInterfaces::Implementation::AddInterface()
 {
   this->sim_interface_handles_.push_back(
