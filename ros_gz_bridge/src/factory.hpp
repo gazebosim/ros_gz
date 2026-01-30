192da4c1825a8b48b2ce0cee93fbd5485bd41703
Ian Chen <ichen@openrobotics.org>
2026-01-30 23:42:54 +0000

    Add override_frame_id parameter
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- ros_gz_bridge/src/factory.hpp
+++ ros_gz_bridge/src/factory.hpp
@@ -117,16 +117,16 @@ public:
     const std::string & topic_name,
     size_t /*queue_size*/,
     rclcpp::PublisherBase::SharedPtr ros_pub,
-    bool override_timestamps_with_wall_time)
+    const BridgeHandleGzToRosParameters & gz_to_ros_parameters)
   {
     auto pub = std::dynamic_pointer_cast<rclcpp::Publisher<ROS_T>>(ros_pub);
     if (pub == nullptr) {
       return;
     }
     std::function<void(const GZ_T &)> subCb =
-      [this, pub, override_timestamps_with_wall_time](const GZ_T & _msg)
+      [this, pub, gz_to_ros_parameters](const GZ_T & _msg)
       {
-        this->gz_callback(_msg, pub, override_timestamps_with_wall_time);
+        this->gz_callback(_msg, pub, gz_to_ros_parameters);
       };
 
     // Ignore messages that are published from this bridge.
@@ -157,17 +157,20 @@ protected:
   void gz_callback(
     const GZ_T & gz_msg,
     std::shared_ptr<rclcpp::Publisher<ROS_T>> ros_pub,
-    bool override_timestamps_with_wall_time)
+    const BridgeHandleGzToRosParameters & gz_to_ros_parameters)
   {
     ROS_T ros_msg;
     convert_gz_to_ros(gz_msg, ros_msg);
     if constexpr (has_header<ROS_T>::value) {
-      if (override_timestamps_with_wall_time) {
+      if (gz_to_ros_parameters.override_timestamps_with_wall_time) {
         auto now = std::chrono::system_clock::now().time_since_epoch();
         auto ns =
           std::chrono::duration_cast<std::chrono::nanoseconds>(now).count();
         ros_msg.header.stamp.sec = ns / 1e9;
         ros_msg.header.stamp.nanosec = ns - ros_msg.header.stamp.sec * 1e9;
+      }
+      if (!gz_to_ros_parameters.override_frame_id.empty()) {
+        ros_msg.header.frame_id = gz_to_ros_parameters.override_frame_id;
       }
     }
     ros_pub->publish(ros_msg);
